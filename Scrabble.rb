d90d494691d019ec8278c042db2a558aeb422bc0
Briana Eng <brianaeng@gmail.com>
2016-09-02 13:54:59 -0700

    More comments

--- Scrabble.rb
+++ Scrabble.rb
@@ -1,4 +1,5 @@
 module Scrabble
+  #Stores the values for specific letters
   SCORE_HASH = {
     %w(a e i o u l n r s t) => 1,
     %w(d g ) => 2,
@@ -9,5 +10,6 @@ module Scrabble
     %w(q z) => 10
     }
 
+    #Stores the default number of each letter
     TILEBAG = (["a"] * 9) + (["b"] * 2) + (["c"] * 2) + (["d"] * 4) + (["e"] * 12) + (["f"] * 2) + (["g"] * 3) + (["h"] * 2) + (["i"] * 9) + (["j"] * 1) + (["k"] * 1) + (["l"] * 4) + (["m"] * 2) + (["n"] * 6) + (["o"] * 8) + (["p"] * 2) + (["q"] * 1) + (["r"] * 6) + (["s"] * 4) + (["t"] * 6) + (["u"] * 4) + (["v"] * 2) + (["w"] * 2) + (["x"] * 1) + (["y"] * 2) + (["z"] * 1)
 end
