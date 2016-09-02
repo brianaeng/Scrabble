fb1063a9fc37899caffb52a097a2b6db225ecaa9
Briana Eng <brianaeng@gmail.com>
2016-09-02 13:27:33 -0700

    Moved tiles

--- lib/TileBag.rb
+++ lib/TileBag.rb
@@ -1,9 +1,11 @@
+require_relative '../Scrabble'
 
 class Scrabble::TileBag
   attr_reader :tiles
   def initialize
-    @tiles = []
-    @tiles += (["a"] * 9) + (["b"] * 2) + (["c"] * 2) + (["d"] * 4) + (["e"] * 12) + (["f"] * 2) + (["g"] * 3) + (["h"] * 2) + (["i"] * 9) + (["j"] * 1) + (["k"] * 1) + (["l"] * 4) + (["m"] * 2) + (["n"] * 6) + (["o"] * 8) + (["p"] * 2) + (["q"] * 1) + (["r"] * 6) + (["s"] * 4) + (["t"] * 6) + (["u"] * 4) + (["v"] * 2) + (["w"] * 2) + (["x"] * 1) + (["y"] * 2) + (["z"] * 1)
+    @tiles = Scrabble::TILES
+    # @tiles = []
+    # @tiles += (["a"] * 9) + (["b"] * 2) + (["c"] * 2) + (["d"] * 4) + (["e"] * 12) + (["f"] * 2) + (["g"] * 3) + (["h"] * 2) + (["i"] * 9) + (["j"] * 1) + (["k"] * 1) + (["l"] * 4) + (["m"] * 2) + (["n"] * 6) + (["o"] * 8) + (["p"] * 2) + (["q"] * 1) + (["r"] * 6) + (["s"] * 4) + (["t"] * 6) + (["u"] * 4) + (["v"] * 2) + (["w"] * 2) + (["x"] * 1) + (["y"] * 2) + (["z"] * 1)
   end
 
   def draw_tiles(num)
@@ -24,6 +26,6 @@ class Scrabble::TileBag
 end
 #
 #
-# bag1 = TileBag.new
+bag1 = Scrabble::TileBag.new
 # print bag1.draw_tiles(3)
-# print bag1.tiles
+print bag1.tiles
